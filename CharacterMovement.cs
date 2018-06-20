bcae3cb3622432d64cdb53e332de11c7bb1f5ad7
stalry221 <bartas@hotmail.no>
2018-06-20 04:20:40 +0200

    Update CharacterMovement.cs

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -1,1 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+
+public class CharacterMovement : Monobehaviour {
+  
+  // Use this for initalization
+  void Start() {
+  
+  }
+  
+  // Update is called once per frame
+  void Update() {
+  
+  }
+  
+}
